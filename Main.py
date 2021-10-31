fca1384a358abd7926f77639071640711c436fa3
Quu <91326783+Quu23@users.noreply.github.com>
2021-10-31 14:31:35 +0900

    dekita
    いい感じにできたよ

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
-from objects import*
+from objects import Chara as ch
+
 
 def main():
-    a = NomalChara(1,"aa")
-
+    a = ch.Chara(1,"aa")
+    print(a.id)
+    i = ch.NomalChara(12,"ss")
+    print(i.name)
 
 if __name__ == "__main__":
     main()
--- objects/Chara.py
+++ objects/Chara.py
@@ -1,10 +1,11 @@
 class Chara:
-    #コンストラクタ
+    #あらゆる生き物
     def __init__(self,id,name):
         self.id = id
         self.name = name
-class NomalChara(Chara):
 
+class NomalChara(Chara):
+    #村人など　戦わない生き物
     def __init__(self,id,name):
-        super.__init__(id,name)
+        super().__init__(id,name)
 
