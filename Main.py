8f84cda21b4959e5ba24bbe8132d2023df235c1e
Quu <91326783+Quu23@users.noreply.github.com>
2021-11-19 06:47:55 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,12 +2,7 @@ from objects import Chara as ch
 
 
 def main():
-    a = ch.Chara(1,"aa")
-    print(a.id)
-    i = ch.NomalChara(12,"ss")
-    print(i.name)
-    murabit = ch.NomalChara(221,"baka")
-    print(murabit.id)
+    print("aa")
 
 if __name__ == "__main__":
     main()
